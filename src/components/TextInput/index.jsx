f8b3f2e6ef6b5147fe71f2c5da015e5fd0ac6f6e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-09-28 23:41:05 -0300

    feat: `TextInput` component created.

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,17 @@
+import props from 'prop-types';
+import './styles.css';
+
+export const TextInput = ({ searchValue, handleChange }) => (
+  <input
+    className="text-input"
+    onChange={handleChange}
+    value={searchValue}
+    type="search"
+    placeholder="Type your search"
+  />
+);
+
+TextInput.propTypes = {
+  searchValue: props.string.isRequired,
+  handleChange: props.func.isRequired,
+};
