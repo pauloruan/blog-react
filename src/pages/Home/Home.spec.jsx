7145b5389ef17e4b6d9500b86c78caca0564e1e4
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-10-22 22:05:38 -0300

    test: `Home` page testing.

---
+++ src/pages/Home/Home.spec.jsx
@@ -0,0 +1,126 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { Home } from '.';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (_req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+        {
+          userId: 4,
+          id: 4,
+          title: 'title4',
+          body: 'body4',
+          url: 'img4.jpg',
+        },
+        {
+          userId: 5,
+          id: 5,
+          title: 'title5',
+          body: 'body5',
+          url: 'img5.jpg',
+        },
+        {
+          userId: 6,
+          id: 6,
+          title: 'title6',
+          body: 'body6',
+          url: 'img6.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+describe('<Home />', () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render search, posts and load more', async () => {
+    const { debug } = render(<Home />);
+    const noMorePosts = screen.getByText(/Não existem posts./i);
+
+    expect.assertions(3);
+    await waitForElementToBeRemoved(noMorePosts);
+
+    debug();
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(6);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText(/Não existem posts./i);
+
+    expect.assertions(19);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title4' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title5' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title6' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title7' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'Search value: title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title4' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title5' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title6' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title7' })).not.toBeInTheDocument();
+    userEvent.type(search, 'post does not exist');
+
+    expect(screen.getByText(/Não existem posts./i)).toBeInTheDocument();
+  });
+});
--- src/templates/Home/Home.test.js
+++
@@ -1,3 +0,0 @@
-it('is a dummy test', () => {
-  expect(true).toBe(true);
-});
