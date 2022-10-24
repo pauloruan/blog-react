c6cd42e3b273c2cbb6aaa6fccd659f7d75af3320
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-10-24 11:02:40 -0300

    perf: `globalStyles.css` renamed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/globalStyles.css';
+import './styles/global-styles.css';
 import { Home } from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
